4ca602d5399e24e75503e64f3b31cf9a9ef94638
Joshua Pfosi <joshpfosi@elbrys.com>
2014-08-23 19:50:04 -0400

    bug fix 2.0

--- app/assets/javascripts/routes/jobs_route.js
+++ app/assets/javascripts/routes/jobs_route.js
@@ -1,17 +1,4 @@
 App.JobsRoute = Em.Route.extend({
-  beforeModel: function() {
-    var route = this;
-
-    // go to loading while post request is processed,
-    // if signedIn, go to jobs, otherwise go to index
-    route.transitionTo('loading');
-    Ember.$.post('/users/sign_in', function() {
-      route.controllerFor('application').set('signedIn', true);
-    }).fail(function() {
-      route.transitionTo('index');
-      route.controllerFor('application').set('signedIn', false);
-    });
-  },
   model: function() {
     return this.store.find('job').then(function(jobs) {
       return jobs;
