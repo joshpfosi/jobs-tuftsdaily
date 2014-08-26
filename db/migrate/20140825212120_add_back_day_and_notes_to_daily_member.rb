82d33ac013be60c7c8c16f5c80db8df8d947ab2c
Joshua Pfosi <joshpfosi@elbrys.com>
2014-08-26 15:36:20 -0400

    changed notes to text

--- db/migrate/20140825212120_add_back_day_and_notes_to_daily_member.rb
+++ db/migrate/20140825212120_add_back_day_and_notes_to_daily_member.rb
@@ -1,6 +1,6 @@
 class AddBackDayAndNotesToDailyMember < ActiveRecord::Migration
   def change
     add_column :daily_members, :back_day, :string
-    add_column :daily_members, :notes, :string
+    add_column :daily_members, :notes, :text
   end
 end
