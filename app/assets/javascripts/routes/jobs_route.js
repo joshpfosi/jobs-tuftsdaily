50db27d30f5ac7f178e0fd6cbf23e61ed1d90108
Joshua Pfosi <joshpfosi@elbrys.com>
2014-09-04 16:26:03 -0400

    fixed publish date archiving bug (temp trial)

--- app/assets/javascripts/routes/jobs_route.js
+++ app/assets/javascripts/routes/jobs_route.js
@@ -3,7 +3,11 @@ App.JobsRoute = Em.Route.extend({
     this.store.find('job').then(function(jobs) {
       jobs.forEach(function(job) {
         var currentTime = new Date().getTime(),
-            publishDate = job.get('publishDate').getTime();
+            publishDate = job.get('publishDate');
+      if (!publishDate) throw "publishDate was null - bad job input";
+      publishDate = publishDate.getTime();
+      console.log('publishDate === ' + publishDate);
+      console.log('currentTime === ' + currentTime);
         if (publishDate < currentTime) { // past publish date
           job.set('state', 6);           // archive the job
           job.save();
