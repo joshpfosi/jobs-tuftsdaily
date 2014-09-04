dc64381e958583aa92e7fdbb09e57bb5cfd70168
Joshua Pfosi <joshpfosi@elbrys.com>
2014-09-04 09:18:13 -0400

    fixed column sort bug

--- app/assets/javascripts/routes/jobs_route.js
+++ app/assets/javascripts/routes/jobs_route.js
@@ -1,10 +1,17 @@
 App.JobsRoute = Em.Route.extend({
+  beforeModel: function() {
+    this.store.find('job').then(function(jobs) {
+      jobs.forEach(function(job) {
+        var currentTime = new Date().getTime(), publishDate = job.get('publishDate');
+        if (publishDate < currentTime) { // past publish date
+          job.set('state', 6);           // archive the job
+          job.save();
+        }
+      });
+    });
+  },
   model: function() {
     return this.store.filter('job', {state: 0}, function(job) {
-      var currentTime = new Date().getTime(), publishDate = job.get('publishDate');
-      if (publishDate < currentTime) { // past publish date
-        job.set('state', 6);           // archive the job
-      }
       return job.get('state') !== 6;
     });
   },
--- app/assets/javascripts/views/jobs_collection_view.js
+++ app/assets/javascripts/views/jobs_collection_view.js
@@ -1,6 +1,6 @@
 App.JobsCollection = Em.CollectionView.extend({
   tagName: 'tbody',
-  content: Em.computed.alias('controller.content'),
+  content: Em.computed.alias('controller.filteredJobs'),
   itemViewClass: App.JobView,
   emptyView: Ember.View.extend({
     template: Ember.Handlebars.compile("<tbody><tr><td colspan=12><em>You need some jobs!</em></td></tr></tbody>")
