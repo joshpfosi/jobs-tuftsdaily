901f4a66ebf73f1d9dcd3ab00ec98a8e0ac9465a
Joshua Pfosi <joshpfosi@elbrys.com>
2014-09-08 09:05:07 -0400

    changed archive date to be 24 hours passed publish date

--- app/assets/javascripts/routes/jobs_route.js
+++ app/assets/javascripts/routes/jobs_route.js
@@ -2,7 +2,7 @@ App.JobsRoute = Em.Route.extend({
   beforeModel: function() {
     this.store.find('job').then(function(jobs) {
       jobs.forEach(function(job) {
-        var currentTime = new Date().getTime(),
+        var currentTime = new Date().getTime() + 24 * 60 * 60 * 1000;
             publishDate = new Date(job.get('publishDate')).getTime();
         if (publishDate < currentTime) { // past publish date
           job.set('state', 6);           // archive the job
