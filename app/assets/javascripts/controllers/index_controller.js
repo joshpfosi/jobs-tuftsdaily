d8e99d17fd21653753c6f4f92d00fc8ee5add8c4
Joshua Pfosi <joshpfosi@elbrys.com>
2014-09-09 22:36:24 -0400

    made full name, email, phone number, and contact not required

--- app/assets/javascripts/controllers/index_controller.js
+++ app/assets/javascripts/controllers/index_controller.js
@@ -70,25 +70,9 @@ App.IndexController = Em.ObjectController.extend({
     }
   },
   validations: {
-    fullName: {
-      regex: /^[A-Za-z0-9 ]{3,20}$/, 
-      message: "Enter your full name (min of 3 characters, max of 20)"
-    },
-    email: {
-      regex: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
-      message: "Enter a valid email"
-    },
-    phone: {
-      regex: /\d\d\d \d\d\d \d\d\d\d/,
-      message: "Follow the placeholder exactly!"
-    },
     title: {
       regex: /.*/,
       message: "Enter a title for the job"
-    },
-    contact: {
-      regex: /.*/,
-      message: "Enter contact information for your subject"
     },
     section: {
       regex: /.*/,
