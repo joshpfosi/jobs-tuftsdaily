5afe41e16ee80815a3805488d8513ca34e054d56
Josh Pfosi <joshpfosi@gmail.com>
2015-01-17 00:37:00 -0500

    fixed jobs.js modals

--- ember/app/controllers/jobs.js
+++ ember/app/controllers/jobs.js
@@ -22,62 +22,31 @@ function sortColumns(column, array) {
   }
 }
 
-function mailJobReject() {
-  var controller = this,
-      job        = this.get('selectedJobs')[0], 
-      deadline   = job.get('dueDate'),
-      email      = job.get('email'),
-      data = {
-        email:        email,
-        subject:      this.get('subject'),
-        name:         job.get('fullName'),
-        coverage_type: job.get('coverageType'),
-        title:        job.get('title'),
-        deadline:     deadline,
-        timestamp:    job.get('createdAt'),
-        details:      job.get('details'),
-        reason:       this.get('reason'),
-        id:           job.get('id')
-      };
+function generateSubjectAssign(coverageType, deadline) {
+  return "Tufts Daily Photo Assignment: " + coverageType + " due on " + deadline;
+}
 
-  Ember.$.ajax({
-    type: "POST",
-    url: 'api/mail_job?type=reject',
-    data: data,
-    success: function() {
-      controller.send('closeMailModal'); // clear the input fields
-      job.set('selected', false); // uncheck the check box
-      job.set('state', 2); // reject it
+function generateBodyAssign(title, name, coverageType, contact, deadline, loc, time, date, details) {
+  return "Dear " + name + ",\n\nPlease cover the following assignment and let me know if you are unable to.\n\nSlug: " + title + "\n\nEvent Details:\n\nCoverage type: <%= @coverage_type %>\nContact information for the subject: " + contact + "\nDue on the Photoshelter server by: " + deadline + "\n\nWhere: " + location + "\nWhen: " + date + " " + time + "\n\nDetails: " + details + "\nThank you for working on this assignment. If you have any questions please call Nick at 603-686-3733 or reply to this email. Please deliver all images onto the Photoshelter server via FTP by the specified deadline with captions, keywords, and proper toning. Full sized JPGs will suffice.\n\nThank you,\n\nThe Tufts Daily Photo Team\n\n";
+}
 
-      // clear associations
-      var member = job.get('daily_member');
-      // if assigned, remove job from daily_member and daily_member from job
-      if (member !== null) { 
-        member.get('jobs').removeObject(job);
-        member.save();
-        job.set('daily_member', null);
-      }
-      job.save();
+function generateSubjectReject(coverageType) {
+  return "Your request for " + coverageType + " needs more detail";
+}
 
-      controller.notify.success('Successfully sent email to ' + job.get('email') + ' regarding job ' + job.get('title') + '.');
-    },
-    error: function() {
-      controller.notify.danger('Failed to send email to ' + email + ' regarding job ' + job.get('title') + '.');
-    },
-    dataType: 'json'
-  });
-  Bootstrap.ModalManager.close('mailModal');
+function generateBodyReject(name, coverageType, title, details, deadline, timestamp, id) {
+  return "Dear " + name + ",\n\nYou have submitted a request:\n\nTitle: " + title + "\nCoverage Type: " + coverageType + "\nDescription: " + details + "\nDeadline: " + deadline + "\nSubmitted on: " + timestamp + "\n\nThank you for taking the time to do this. Unfortunately we are unable to cover your request.\n\n[Reason for rejection]\n\nPlease reply with any modification or additional ideas you may have, or edit the job request directly at:\n\n http://jobs-tuftsdaily.herokuapp.com/#/job/" + id + "\n\nThank you,\n\nThe Tufts Daily Photo Team\n\n";
 }
 
 export default Ember.ArrayController.extend({
-  mailJobAssign: [
-      Ember.Object.create({title: 'Submit', clicked: "mailJobAssign"}),
-      Ember.Object.create({title: 'Cancel', clicked: 'closeMailModal', dismiss: 'modal'})
-  ],
-  mailJobReject: [
-      Ember.Object.create({title: 'Submit', clicked: "mailJobReject"}),
-      Ember.Object.create({title: 'Cancel', clicked: 'closeMailModal', dismiss: 'modal'})
-  ],
+  //mailJobAssign: [
+  //    Ember.Object.create({title: 'Submit', clicked: "mailJobAssign"}),
+  //    Ember.Object.create({title: 'Cancel', clicked: 'closeMailModal', dismiss: 'modal'})
+  //],
+  //mailJobReject: [
+  //    Ember.Object.create({title: 'Submit', clicked: "mailJobReject"}),
+  //    Ember.Object.create({title: 'Cancel', clicked: 'closeMailModal', dismiss: 'modal'})
+  //],
   columns: [
      Ember.Object.create({value: '',             name: ''          }), 
      Ember.Object.create({value: 'id',           name: '#'         }),
@@ -113,9 +82,11 @@ export default Ember.ArrayController.extend({
 
   selectedJobs: Ember.computed.filterBy('content', 'selected'),
   selectedDailyMember: null,
-  isSelectedJobs: Ember.computed.empty('selectedJobs'),
-  isSelectedDailyMember: Ember.computed.empty('selectedDailyMember'),
-  isAssignable: Ember.computed.or('isSelectedDailyMember', 'isSelectedJobs'),
+  isSelectedJobs: Ember.computed.notEmpty('selectedJobs'),
+  isNotSelectedJobs: Ember.computed.not('isSelectedJobs'),
+  isSelectedDailyMember: Ember.computed.notEmpty('selectedDailyMember'),
+  isAssignable: Ember.computed.and('isSelectedDailyMember', 'isSelectedJobs'),
+  isNotAssignable: Ember.computed.not('isAssignable'),
 
   actions: {
     deleteJob: function(job) {
@@ -136,35 +107,25 @@ export default Ember.ArrayController.extend({
         job.save();
       });
     },
-    showMailModal: function(type) {
+    setupMailAssign: function() {
       var job = this.get('selectedJobs')[0].get('data'), 
-          deadline = job.dueDate;
-      if (type === "assign") {
-        var member = this.get('selectedDailyMember.data'),
-            name = member.name;
-        this.set('email', member.email);
+      deadline = job.dueDate;
 
-        this.set('subject', generateSubjectAssign(job.coverageType, deadline));
-        this.set('body', generateBodyAssign(job.title, name, job.coverageType, job.contact, 
-              deadline, job.loc, job.time, job.date, job.details));
+      var member = this.get('selectedDailyMember.data'),
+          name = member.name;
+      this.set('email', member.email);
 
-        Bootstrap.ModalManager.open('mailModal', 'Assign Job: ' +
-            job.title, 'mail_assign', this.mailJobAssign, this);
-      }
-      else { // type === 'reject'
-        this.set('email', job.email);
-        this.set('subject', generateSubjectReject(job.coverageType));
-        this.set('body', generateBodyReject(job.fullName, job.coverageType, 
-              job.title, job.details, deadline, job.createdAt, job.id));
-
-        Bootstrap.ModalManager.open('mailModal', 'Reject Job: ' + job.title, 
-            'mail_reject', this.mailJobReject, this);
-      }
+      this.set('subject', generateSubjectAssign(job.coverageType, deadline));
+      this.set('body', generateBodyAssign(job.title, name, job.coverageType, job.contact, 
+            deadline, job.loc, job.time, job.date, job.details));
     },
-    closeMailModal: function() {
-      this.set('reason', '');
-      this.set('subject', '');
-      this.set('body', '');
+    setupMailReject: function() {
+      var job = this.get('selectedJobs')[0].get('data'), 
+      deadline = job.dueDate;
+      this.set('email', job.email);
+      this.set('subject', generateSubjectReject(job.coverageType));
+      this.set('body', generateBodyReject(job.fullName, job.coverageType, 
+            job.title, job.details, deadline, job.createdAt, job.id));
     },
     mailJobAssign: function() {
       var controller = this,
@@ -188,16 +149,15 @@ export default Ember.ArrayController.extend({
 
       Ember.$.ajax({
         type: "POST",
-        url: '/mail_job?type=assign',
+        url: 'api/mail_job?type=assign',
         data: data,
         success: function() {
-          controller.send('closeMailModal'); // clear the input fields
           job.set('selected', false); // uncheck the check box
           job.set('state', 1); // assign it
 
           // establish associations
           var currentMem = job.get('daily_member'); // unassign old member
-          if (currentMem !== null) {
+          if (currentMem !== undefined) {
             currentMem.get('jobs').removeObject(job); 
           }
           job.set('daily_member', member);                // assign new one
@@ -213,8 +173,51 @@ export default Ember.ArrayController.extend({
         },
         dataType: 'json'
       });
-      return Bootstrap.ModalManager.close('mailModal');
     },
-    mailJobReject: mailJobReject
+    mailJobReject: function() {
+      var controller = this,
+      job        = this.get('selectedJobs')[0], 
+      deadline   = job.get('dueDate'),
+      email      = job.get('email'),
+      data = {
+        email:        email,
+        subject:      this.get('subject'),
+        name:         job.get('fullName'),
+        coverage_type: job.get('coverageType'),
+        title:        job.get('title'),
+        deadline:     deadline,
+        timestamp:    job.get('createdAt'),
+        details:      job.get('details'),
+        reason:       this.get('reason'),
+        id:           job.get('id')
+      };
+
+      Ember.$.ajax({
+        type: "POST",
+        url: 'api/mail_job?type=reject',
+        data: data,
+        success: function() {
+          job.set('selected', false); // uncheck the check box
+          job.set('state', 2); // reject it
+
+          // clear associations
+          var member = job.get('daily_member');
+          // if assigned, remove job from daily_member and daily_member from job
+          if (member !== undefined) { 
+            member.get('jobs').removeObject(job);
+            member.save();
+            job.set('daily_member', null);
+          }
+          job.save();
+
+          controller.notify.success('Successfully sent email to ' + job.get('email') + ' regarding job ' + job.get('title') + '.');
+        },
+        error: function() {
+          controller.notify.danger('Failed to send email to ' + email + ' regarding job ' + job.get('title') + '.');
+        },
+        dataType: 'json'
+      });
+    }
+
   },
 });
