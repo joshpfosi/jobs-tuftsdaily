e5869c21a1286fe216f8d85431e2565e3fa25b3f
Josh Pfosi <joshpfosi@gmail.com>
2015-01-21 10:10:46 -0500

    Patch to CSRF issue

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -1,5 +1,11 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
-    namespace: 'api'
+  namespace: 'api',
+  headers: Ember.computed(function(){
+    var token = Ember.$('meta[name="csrf-token"]').attr('content');
+
+    return {"X-CSRF-Token": token};
+  })
 });
