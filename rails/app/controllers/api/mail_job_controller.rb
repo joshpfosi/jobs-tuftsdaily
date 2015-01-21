0bd609b932b7d65cbddd478459dc1f1de8ed970b
Josh Pfosi <joshpfosi@gmail.com>
2015-01-21 17:03:22 -0500

    Fixed #deliver deprecation error

--- rails/app/controllers/api/mail_job_controller.rb
+++ rails/app/controllers/api/mail_job_controller.rb
@@ -4,16 +4,16 @@ class Api::MailJobController < ApplicationController
   def create
     data = params
     if params[:type] == 'assign'
-      AutoMailer.mail_job_assign(data).deliver
+      AutoMailer.mail_job_assign(data).deliver_now
     elsif params[:type] == 'reject'
-      AutoMailer.mail_job_reject(data).deliver
+      AutoMailer.mail_job_reject(data).deliver_now
     elsif params[:type] == 'members'
-      AutoMailer.mail_job_members(data).deliver
+      AutoMailer.mail_job_members(data).deliver_now
     elsif params[:type] == ' job'
-      AutoMailer.mail_job(data[:job], "npfosi@gmail.com").deliver
-      AutoMailer.mail_job(data[:job], data[:editorEmail]).deliver
+      AutoMailer.mail_job(data[:job], "npfosi@gmail.com").deliver_now
+      AutoMailer.mail_job(data[:job], data[:editorEmail]).deliver_now
     else # type == update_job
-      AutoMailer.mail_job(data, "npfosi@gmail.com").deliver
+      AutoMailer.mail_job(data, "npfosi@gmail.com").deliver_now
     end
     render json: "ok", status: 204
   end
