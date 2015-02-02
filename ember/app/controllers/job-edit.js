5df3c335e70a6776d6ace6b8b00ace5a1a4859bd
Josh Pfosi <joshpfosi@gmail.com>
2015-02-02 13:44:04 -0500

    Fixed edit job redirection bug

--- ember/app/controllers/job-edit.js
+++ ember/app/controllers/job-edit.js
@@ -3,19 +3,25 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   isOther: Ember.computed.equal('coverageType', 'Other'),
 
+  isntValid: Ember.computed.not('model.isValid'),
   actions: {
-    save: function(model) {
+    save: function(callback) {
       if (this.get('isOther')) {
         this.set('coverageType', this.get('coverageTypeOther'));
       }
 
+      var model = this.get('model');
       var controller = this;
       model.set('state', 0); // make it unassigned
-      model.save().then(function(job) {
-        Ember.$.ajax({
-          type: 'POST',
-          url: 'api/mail_job?type=update_job',
-          data: { job: job },
+
+      var promise = model.save().then(function(model) {
+        controller.notify.success('Successfully saved the job.');
+        return Ember.$.ajax({
+          type: 'POST', url: 'api/mail_job?type=update_job',
+          data: { 
+            title: model.get('title'),
+            id:    model.get('id')
+          },
           success: function() {
             controller.notify.success('Successfully notified administrator.');
             controller.transitionToRoute('jobs');
@@ -25,11 +31,11 @@ export default Ember.ObjectController.extend({
           },
           dataType: 'json'
         });
-
-        controller.notify.success('Successfully saved the job.');
       }, function() {
         controller.notify.alert('Failed to save the job.');
       });
+
+      callback(promise);
     }
   },
 });
