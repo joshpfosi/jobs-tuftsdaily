6b5f20ef0ea77da7bb229952a33c3509051e6758
Josh Pfosi <joshpfosi@gmail.com>
2015-02-04 19:33:38 -0500

    Removed projects

--- ember/app/models/project.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  author: DS.attr('string'),
-  startDate: DS.attr('string'),
-  publishDate: DS.attr('string'),
-  notes: DS.attr('string'),
-});
--- ember/app/router.js
+++ ember/app/router.js
@@ -8,7 +8,6 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('jobs');
   this.route('settings');
-  this.route('projects');
   this.route('archive');
   this.route('stock');
   this.route('job_edit', { path: 'job/:job_id' });
