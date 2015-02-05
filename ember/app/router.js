f7eab048c08fd3e7f1d8128c84b591f46d0eb590
Josh Pfosi <joshpfosi@gmail.com>
2015-02-04 19:46:34 -0500

    Removed Research Requests page

--- ember/app/router.js
+++ ember/app/router.js
@@ -9,7 +9,6 @@ Router.map(function() {
   this.route('jobs');
   this.route('settings');
   this.route('archive');
-  this.route('stock');
   this.route('job_edit', { path: 'job/:job_id' });
   this.resource('mail_job');
 });
