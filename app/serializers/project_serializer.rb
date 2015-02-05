f2246d9e8e2b20b90919381be65122151a6dcb73
Josh Pfosi <joshpfosi@gmail.com>
2015-02-04 19:33:38 -0500

    Removed projects

--- app/serializers/project_serializer.rb
+++
@@ -1,3 +0,0 @@
-class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :publish_date, :start_date, :notes
-end
--- spec/factories/projects.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :project do
-    title "Test Name"
-    author { "#{Faker::Name.first_name} #{Faker::Name.last_name}" }
-    publish_date { Faker::Time.forward(30) }
-    start_date { Faker::Time.forward(25) }
-    notes { Faker::Lorem.paragraph(3) }
-  end
-end
